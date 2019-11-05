d861030a3c0c91f498dcd6c965faee6e2059a496
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-11-05 15:56:00 +0100

    GatedComputing: add testcase with bigger bytecode (#14)

--- test/GatedComputing.js
+++ test/GatedComputing.js
@@ -10,43 +10,49 @@ const Runtime = require('./../js/NutBerryRuntime.js');
 describe('GatedComputing', async function () {
   const provider = new ethers.providers.JsonRpcProvider(`http://localhost:${process.env.RPC_PORT}`);
 
-  it('patching jumps and simple opcodes should work', async () => {
-    const wallet = await provider.getSigner(0);
-    const _factory = new ethers.ContractFactory(
-      GatedComputing.abi,
-      GatedComputing.bytecode,
-      wallet,
-    );
-    const contract = await _factory.deploy();
-    await contract.deployTransaction.wait();
+  function round (i, bytecode) {
+    it(`Round ${i} patching jumps and simple opcodes should work`, async () => {
+      const wallet = await provider.getSigner(0);
+      const _factory = new ethers.ContractFactory(
+        GatedComputing.abi,
+        GatedComputing.bytecode,
+        wallet,
+      );
+      const contract = await _factory.deploy();
+      await contract.deployTransaction.wait();
 
-    let bytecode = TestContract.deployedBytecode;
+      const bytecodeLength = (bytecode.length - 2) / 2;
+      const addr = await provider.send('eth_call', [{ to: contract.address, data: bytecode }, 'latest']);
+      const tx = await(await wallet.sendTransaction(
+        {
+          to: contract.address,
+          data: bytecode,
+          gasLimit: 0xffffffff,
+        }
+      )).wait();
 
-    const addr = await provider.send('eth_call', [{ to: contract.address, data: bytecode }, 'latest']);
-    const tx = await(await wallet.sendTransaction(
-      {
-        to: contract.address,
-        data: bytecode,
-        gasLimit: 0xffffffff,
-      }
-    )).wait();
+      const gasUsed = tx.cumulativeGasUsed.toNumber();
+      const patchedBytecode = await provider.getCode(addr);
+      const patchedBytecodeLength = (patchedBytecode.length - 2) / 2;
+      const runState = await new Runtime().run(
+        {
+          code: ethers.utils.arrayify(patchedBytecode),
+          origin: Buffer.alloc(20).fill(0xfa),
+          caller: Buffer.alloc(20).fill(0xfa),
+          address: Buffer.alloc(20).fill(0xfa),
+          data: [],
+        }
+      );
 
-    const gasUsed = tx.cumulativeGasUsed.toNumber();
-    const patchedBytecode = await provider.getCode(addr);
-    const patchedBytecodeLength = (patchedBytecode.length - 2) / 2;
-    const runState = await new Runtime().run(
-      {
-        code: ethers.utils.arrayify(patchedBytecode),
-        origin: Buffer.alloc(20).fill(0xfa),
-        caller: Buffer.alloc(20).fill(0xfa),
-        address: Buffer.alloc(20).fill(0xfa),
-        data: [],
-      }
-    );
+      assert.equal(runState.opName, 'RETURN', 'return-opcode');
+      assert.equal(runState.returnValue.toString('hex'), Buffer.alloc(32).toString('hex'), 'returnValue');
 
-    assert.equal(runState.opName, 'RETURN', 'return-opcode');
-    assert.equal(runState.returnValue.toString('hex'), Buffer.alloc(32).toString('hex'), 'returnValue');
+      console.log({ gasUsed, addr, patchedBytecodeLength, bytecodeLength });
+    });
+  }
 
-    console.log({ gasUsed, addr, patchedBytecodeLength });
-  });
+  let bytecode = TestContract.deployedBytecode;
+  round(1, bytecode);
+  bytecode = bytecode + Buffer.alloc(6000).fill(0xff).toString('hex');
+  round(2, bytecode);
 });
